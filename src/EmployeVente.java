83afe91886b6b1813b04c8bdb06be46834bc280f
Eloise Goncalves Green <eloisegg@gmail.com>
2025-04-28 21:52:39 +0200

    Adding VAT

--- src/EmployeVente.java
+++ src/EmployeVente.java
@@ -1,5 +1,6 @@
 public class EmployeVente extends Employe{
     private double chiffreAffaire;
+    private double vat;
 
     public EmployeVente(String prenom, String nom, int age, double chiffreAffaire){
         super(prenom, nom, age);
@@ -19,5 +20,13 @@ public class EmployeVente extends Employe{
     }
     public void setChiffreAffaire(double chiffreAffaire){
         this.chiffreAffaire = chiffreAffaire;
+    }
+
+    public double getVat() {
+        return vat;
+    }
+
+    public void setVat(double vat) {
+        this.vat = vat;
     }
 }
